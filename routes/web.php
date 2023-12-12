2be657f9265db55ba55c0d11486b32d05fb7e7fc
Shamshad Alam <shamshad.siddiqui25@gmail.com>
2023-12-12 13:20:16 +0530

    Route updated

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Post;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+
+    return view('posts',[
+        'posts' => Post::all()
+    ]);
+
+});
+
+Route::get('posts/{post}',function($slug){
+    // Find a post by its slug and pass it to a view called post
+
+    return view('post',[
+        'post' => Post::find($slug)
+    ]);
+})->where('post','[A-z_\-]+');
